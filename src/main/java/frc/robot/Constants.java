3e04c9fe3846154bad4a03a2b0e9d1fdf66d3996
yassataiseer <yassataiseer@gmail.com>
2022-06-15 11:44:50 -0400

    commit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,8 +7,8 @@ public final class Constants
      * Motor Constants
      */
     public static final int TITAN_ID = 42;
-    public static final int MOTORR = 1;
-    public static final int MOTORL = 3;
+    public static final int MOTORR = 0;
+    public static final int MOTORL = 2;
     public static final int SERVO_C = 0;//servo channel
 
 }
